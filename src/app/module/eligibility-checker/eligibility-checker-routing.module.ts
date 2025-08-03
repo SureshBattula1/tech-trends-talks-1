e37d3ebc83bbde1199ae4feece347ebde0882ed2
murali <nmurali.ab@gmail.com>
2025-08-03 16:35:19 +0530

    m0difier the checker c0de

--- src/app/module/eligibility-checker/eligibility-checker-routing.module.ts
+++ src/app/module/eligibility-checker/eligibility-checker-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EligibilityCheckerComponent } from './eligibility-checker.component';
+import { ComponentSidenavComponent } from '../../core/component-sidenav/component-sidenav.component';
 
 const routes: Routes = [
-  { path: '', component: EligibilityCheckerComponent }
+  {
+     path: '', component: ComponentSidenavComponent, children: [
+        { path: 'checker', component: EligibilityCheckerComponent, data: { breadcrumb: 'Checker' } },
+        { path: '',  redirectTo: 'checker', pathMatch: 'full', data: { breadcrumb: 'Checker' }}
+    ], data: { breadcrumb: 'Eligibility Checker' }
+  }
 ];
 
 @NgModule({
