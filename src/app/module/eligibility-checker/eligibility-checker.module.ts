10342c1699909fdb84da9b95746a2726f4794e0e
Suresh Battula <sureshbattula@Sureshs-MacBook-Air.local>
2025-08-15 00:48:21 +0530

    added social meadia icon ans seo for eligibilty checker

--- src/app/module/eligibility-checker/eligibility-checker.module.ts
+++ src/app/module/eligibility-checker/eligibility-checker.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 import { EligibilityCheckerComponent } from './eligibility-checker.component';
 import { EligibilityCheckerRoutingModule } from './eligibility-checker-routing.module';
 import { NgChartsModule } from 'ng2-charts';
@@ -19,6 +20,7 @@ import { MatSliderModule } from '@angular/material/slider';
     ReactiveFormsModule,
     MatAutocompleteModule,
     MatInputModule,
+    MatIconModule,
     EligibilityCheckerRoutingModule,
     NgChartsModule,
     MatSliderModule
